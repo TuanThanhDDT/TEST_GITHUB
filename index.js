b2109d0f6a584a9667af4a60377fabb454865848
Tuan Thanh <daotuanthanh2529999@gmail.com>
2023-05-02 18:36:21 +0700

    hello 6h36

---
+++ index.js
@@ -0,0 +1,9 @@
+var imgEl = document.getElementById("img__on__off");
+//global
+function batDen(){
+    imgEl.src = "./pic_bulbon.gif";
+}
+
+function tatDen(){
+    imgEl.src = "./pic_bulboff.gif";
+}
